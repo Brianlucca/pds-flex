6875db4e2632cc829765db46aef69315c07fae6b
Brianlucca <brianlucca2002@gmail.com>
2024-05-10 10:17:51 -0300

    added logic if the user is not logged in correctly, if it is false it will render page not found for him.

---
+++ src/screen/requestsPage/index.jsx
@@ -0,0 +1,18 @@
+import PageNotFound from '../../components/pageNotFound'
+
+function RequestsPage() {
+  const isLoggedIn = localStorage.getItem('isPedidos') === 'true'
+
+  if (!isLoggedIn) {
+    return <PageNotFound />
+  } else {
+    return (
+      <>
+        <div>
+          <h1>Tu ta em pedidos, mano, gerencie sua parada</h1>
+        </div>
+      </>
+    )
+  }
+}
+export default RequestsPage
