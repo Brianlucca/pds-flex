f779cb813a9fdecf3f1718b6bd3103f82e0553ab
Brianlucca <brianlucca2002@gmail.com>
2024-05-13 08:23:49 -0300

    feat: added page not found screen information

--- src/components/pageNotFound/index.jsx
+++ src/components/pageNotFound/index.jsx
@@ -1,13 +1,29 @@
 import { useNavigate } from 'react-router-dom'
+import './style.css'
 
 function PageNotFound() {
   const navigate = useNavigate()
 
   return (
-    <div>
-      <h2>oops! pagina não encontrada, tu é viado isso sim</h2>
-      <button onClick={() => navigate('/')}>volte aqui corno</button>
+    <div className="container-page-not-found">
+      <div className="container-content">
+        <div className="content-left">
+          <div className="message-error">
+            <h1>404</h1>
+            <h1>Página não encontrada</h1>
+          </div>
+        </div>
+        <div className="content-right">
+          <div className="return-login">
+            <p>
+              Não consegue encontrar sua página? Realize o seu login novamente
+            </p>
+            <button onClick={() => navigate('/')}>Login</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
+
 export default PageNotFound
